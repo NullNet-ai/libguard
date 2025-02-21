1df2da8aa9eb7a48d58d69c7aab4ecbb64277850
GyulyVGC <gyulyvgc99@gmail.com>
2025-02-21 10:54:48 +0100

    liblogging add runner CLI arg to sample program

---
+++ liblogging/src/bin/simple.rs
@@ -0,0 +1,20 @@
+use nullnet_liblogging::Logger;
+
+fn main() {
+    // read first command line argument
+    let args: Vec<String> = std::env::args().collect();
+    let default_runner = "sample program".to_string();
+    let runner = args.get(1).unwrap_or(&default_runner);
+
+    Logger::init(None, true, vec!["simple"]);
+
+    loop {
+        log::error!("This is an error message from {runner}");
+        log::warn!("This is a warning message from {runner}");
+        log::info!("This is an info message from {runner}");
+        log::debug!("This is a debug message from {runner}");
+        log::trace!("This is a trace message from {runner}");
+
+        std::thread::sleep(std::time::Duration::from_secs(10));
+    }
+}
