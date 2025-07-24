8c16467f70237235bd3826078500ec33e5ae3288
Anton Liashkevich <anton.liashkevich.eng@gmail.com>
2025-07-24 22:15:06 +0000

    Temporarily removed experimental DS proto from compilation

--- server_libraries/libdatastore/build.rs
+++ server_libraries/libdatastore/build.rs
@@ -1,4 +1,5 @@
 const DATASTORE_PROTOBUF_PATH: &str = "./proto/datastore.proto";
+#[allow(unused)]
 const STORE_PROTOBUF_PATH: &str = "./proto/store.proto";
 
 const PROTOBUF_DIR_PATH: &str = "./proto";
@@ -7,7 +8,7 @@ fn main() {
     tonic_build::configure()
         .out_dir("./src")
         .compile_protos(
-            &[DATASTORE_PROTOBUF_PATH, STORE_PROTOBUF_PATH],
+            &[DATASTORE_PROTOBUF_PATH, /*STORE_PROTOBUF_PATH*/],
             &[PROTOBUF_DIR_PATH],
         )
         .expect("Protobuf files generation failed");
